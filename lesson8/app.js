5eb87b4dab4a5bfcc1656ba0406279d4e9848002
yo-mom-gay <eetao.chen@online.island.edu.hk>
2019-02-27 17:33:48 +0800

    lesson 8

--- lesson8/app.js
+++ lesson8/app.js
@@ -8,10 +8,10 @@ function preload(){
 	game.load.image('ground' , 'assets/platform.png');
 	game.load.image('star' , 'assets/star.png');
 	game.load.spritesheet('dude','assets/dude.png' , 32, 48);
-	game.load/spritesheet('baddie', 'assets/baddie.png', 32);
+	game.load.spritesheet('baddie', 'assets/baddie.png', 32);
 }
 
-fucntion create(){
+function create(){
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	game.add.sprite(0,0 'sky');
 
