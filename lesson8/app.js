e961677f7b57d9729616ef1250b7d8341ca348cd
yo-mom-gay <eetao.chen@online.island.edu.hk>
2019-02-27 17:38:20 +0800

    lesson 8

--- lesson8/app.js
+++ lesson8/app.js
@@ -4,7 +4,7 @@ var score = 0;
 var life = 3;
 
 function preload(){
-	game.load.image('sky' , 'assets/sky/png');
+	game.load.image('sky' , 'assets/sky.png');
 	game.load.image('ground' , 'assets/platform.png');
 	game.load.image('star' , 'assets/star.png');
 	game.load.spritesheet('dude','assets/dude.png' , 32, 48);
