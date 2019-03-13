68cac531ef469d199c0c3cf86bdfca8d5b52e02a
yo-mom-gay <eetao.chen@online.island.edu.hk>
2019-03-13 22:10:30 +0800

    lesson10

--- lesson10/app.js
+++ lesson10/app.js
@@ -43,7 +43,7 @@ function create(){
 
 	player = game.add.sprite(32, 400, 'dude');
 		player.animations.add('left', [0,1,2,3], 10, true);
-		player.animation.add('left', [5,6,7,8], 10, true);
+		player.animation.add('right', [5,6,7,8], 10, true);
 		game.physics.arcade.enable(player);
 		player.body.bounce.y = 0.2;
 		player.body.gravity.y = 300;
